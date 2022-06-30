911d891293f0801a35d2c7f67fe88a1546177452
Samy Frey <89148403+samyfrey@users.noreply.github.com>
2022-06-30 14:40:05 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">123,324</div>
+        <div className="current-operand">455,789</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
